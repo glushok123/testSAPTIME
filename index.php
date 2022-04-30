8b541008fa1d80213a08c49b0645a24f6db0c4d9
glushok123 <glushok19999@gmail.com>
2022-04-30 17:35:07 +0300

    4

--- index.php
+++ index.php
@@ -5,18 +5,18 @@
 <section>
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col-lg-4 col-sm-12">
                 <h3>Список фильмов</h3>
                 <select  class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" id="films" size=10>
                   <option selected>&nbsp;</option>
                 </select>
             </div>
-            <div class="col">
+            <div class="col-lg-4 col-sm-12">
                 <h3>Категория</h3>
                 <select  class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" id="categ" size=10 disabled>
                 </select>
             </div>
-            <div class="col">
+            <div class="col-lg-4 col-sm-12 ">
                 <h3>Актёры  <span class="addactor " title="Добавить"><i class="fa fa-plus" aria-hidden="true"></i> </span> <span class="removeactor" title="Удалить"><i class="fa fa-trash-o" aria-hidden="true"></i></span>  <span class="actorcop" title="Скопировать в другой фильм"><i class="fa fa-clone" aria-hidden="true"></i></span></h3>
                 <select  class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" id="actor" size=10 disabled multiple >
                 </select>
